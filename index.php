523f2ac850a8ce9b3b13b0ddc5e92a848923a455
Renal <renalplaycsgo@gmail.com>
2023-12-09 14:36:58 +0300

    finish

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ include("connect.php");
         <input type="file" name="file" id="" class="w-full bg-white rounded border border-gray-300 focus:bg-red-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
       </div>
       <input  type="submit" value="Добавить" name="btn" class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-500 rounded text-lg">
-      <a href="/project/main.php">Перейти к файлам</a>
+      <a href="main.php">Перейти к файлам</a>
     </form>
   </div>
 </section>
--- main.php
+++ main.php
@@ -3,6 +3,7 @@ include("connect.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,65 +14,63 @@ include("connect.php");
   <!-- tailwind -->
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
 
-<?php 
+  <?php
   @include "Header/header.php";
-?>
-
-<div class="panel">
-  <form>
-    <?php
-    $sql = mysqli_query($link,'SELECT * FROM files');
-    foreach($sql as $row){
   ?>
-    <div>
-      <h1 >
-        <?php
-          echo $row['file'];
-        ?>
-      </h1>
-      <a href="main.php?path=file/<?php  echo $row['file']; ?>" class="flex mx-auto w-32 text-white bg-red-500 border-0 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">Cкачать</a>
+
+  <div class="panel">
+    <form>
       <?php
-        }
+      $sql = mysqli_query($link, 'SELECT * FROM files');
+      foreach ($sql as $row) {
       ?>
-    </div>
-  </form>
-</div>
+        <div class="container flex center border-solid border-2 border-red-500 rounded-md p-1 gap-2 text_center">
+          <h1 class=""> <?php
+                echo $row['file'];
+                ?></h1>
+          <a href="main.php?path=file/<?php echo $row['file']; ?>" class="flex  w-32 h-9 text-white bg-red-500 border-0 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">Скачать</a>
+        </div>
+      <?php
+      }
+      ?>
+    </form>
+  </div>
 
 
-<?php
-if(isset($_GET['path']))
-{
-$url = $_GET['path'];//Очистить кэш
+  <?php
+  if (isset($_GET['path'])) {
+    $url = $_GET['path']; //Очистить кэш
 
-//Проверьте, существует ли путь к файлу или нет
-if(file_exists($url)) {
+    //Проверьте, существует ли путь к файлу или нет
+    if (file_exists($url)) {
 
-//Определение информации заголовка
-header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="'.basename($url).'"');
-header('Pragma: public');
+      //Определение информации заголовка
+      header('Content-Description: File Transfer');
+      header('Content-Type: application/octet-stream');
+      header('Content-Disposition: attachment; filename="' . basename($url) . '"');
+      header('Pragma: public');
 
-//Очистить выходной буфер системы
-flush();
+      //Очистить выходной буфер системы
+      flush();
 
-//Считайте размер файла
-readfile($url,true);
+      //Считайте размер файла
+      readfile($url, true);
 
-//Завершить работу со скриптом
-die();
-}
-else{
+      //Завершить работу со скриптом
+      die();
+    } else {
 
-echo '';
-}
-}
-echo '';
+      echo '';
+    }
+  }
+  echo '';
 
-?>
+  ?>
 
 
 </body>
+
 </html>
