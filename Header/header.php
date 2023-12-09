29263ee38219809f4b3aa60ea448307818ac958e
NistonN <nisprogrammer@gmail.com>
2023-12-09 12:54:11 +0300

    push

---
+++ Header/header.php
@@ -0,0 +1,10 @@
+<header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+    <img src="./image/Без названия (2).jpg" alt="logo" class="w-20 rounded">
+      <span class="ml-3 text-xl">Документооборот</span>
+    </a>
+    <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+    </div>
+  </div>
+</header>
